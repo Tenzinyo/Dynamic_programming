78176395a51b2c1b64995ce394424794edc39e30
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-12-28 16:17:56 -0800

    subsets answer

--- unique_paths.py
+++ unique_paths.py
@@ -125,3 +125,49 @@ def get_path(grid):
     path.reverse()
     print(path)
 get_path([[0,2,2,1],[3,1,1,1],[4,4,2,0]])
+
+
+def paint(n):
+    """ 
+        objective function -> paint the house and make sure that the adjacent houses dont have same color
+        base case -> 
+            f(i) 4->  0 0 0 1 -> blue = 0 green = 1
+                    f(i-1) = needs to be blue
+                    f(i-2) = needs to be green
+                    f(i-3) = needs to be blue lets have j as the color 
+                    the next j is the opositite of color before = 1-j 
+        recursive function = f(i,j) = f(i-1,1-j) + f(i-2,1-j)
+                    
+    """
+    dp = [[0 for _ in range(2)] for _ in range(n+1)]
+    dp[1][0] = 1
+    dp[1][1] = 1 
+    dp[2][0] = 2
+    dp[2][1] = 2 
+    for i in range(3,n+1):
+         for j in range(2):
+             dp[i][j] = dp[i-1][1-j] + dp[i-2][1-j]
+    print(dp[n][0] + dp[n][1])
+paint(5)
+
+def subsets(nums):
+    """ 
+        f(0) = []
+        f(1) = f(0) + f(1)
+        f(2) = f(0) + f(1) + f(2)
+        f(3) = f(0) + f(1) + f(2) + f(3)
+    """
+    n = len(nums)
+    dp = [[]]*(n+1)
+    dp[0] = [[]]
+    for i in range(1,n+1):
+        prev = dp[i-1]
+        result = []
+        for p in prev:
+            result.append(p+[nums[i-1]])
+            dp[i] = prev+result
+    return dp[n]
+
+            
+    
+  
